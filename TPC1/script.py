4aca6bbe0193b248a6bf42ece956adcf5bfde1d3
Diogo Marques <a100897@alunos.uminho.pt>
2024-02-05 22:39:08 +0000

    update script

---
+++ TPC1/script.py
@@ -0,0 +1,53 @@
+import traceback
+from os import listdir
+from os.path import isfile, join
+from colorama import Fore, Style
+import xml.etree.ElementTree as ET
+
+
+path = 'texto'
+
+
+try:
+    files = [path + '/' + f for f in listdir(path) if isfile(join(path, f))]
+
+except FileNotFoundError:
+    print(f'{Fore.RED}{Style.BRIGHT}Invalid folder: {path}{Style.RESET_ALL}')
+
+except Exception:
+    traceback.print_exc()
+
+
+indexList = list()
+
+for file in files:
+
+    tree = ET.parse(file)
+    root = tree.getroot()
+    numbers = root.findall('.//número')
+    names = root.findall('.//nome')
+
+    if len(names) > 0 and len(numbers) > 0:
+        indexList.append((int(numbers[0].text),names[0].text))
+    
+
+indexList = sorted(indexList, key = lambda x : x[0])
+
+
+with open('.base.html') as baseFile:
+    indexFileContent = baseFile.read()
+
+
+indexFileContent = indexFileContent.replace('?????', 'Página Inicial', 1)
+indexFileContent = indexFileContent.replace('?????', 'Índice das Ruas', 1)
+
+bodyContent = '<ul class="pagination">'
+
+for item in indexList:
+    bodyContent += f'<li><a href="https://www.google.com/">{item[1]}</a></li>'
+
+bodyContent += '</ul>'
+indexFileContent = indexFileContent.replace('?????', bodyContent)
+
+with open('index.html', 'w') as indexFile:
+    indexFile.write(indexFileContent)
